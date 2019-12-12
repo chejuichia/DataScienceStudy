1f5d2c1ca8f08a508f8d220b775fbdb4721c46ea
juichiaholland <51493603+juichiaholland@users.noreply.github.com>
2019-12-12 15:35:49 -0500

    Add files via upload

---
+++ Capstone Project 2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A data science study in demand forecasting',
+    author='Juichia Holland',
+    license='BSD-3',
+)
