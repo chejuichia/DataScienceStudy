371b2bb86b2c128ec9ae71abdc0fe9dc4dd078b7
juichiaholland <51493603+juichiaholland@users.noreply.github.com>
2020-03-26 21:01:53 -0400

    Add files via upload

---
+++ ultimate_challenge/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='data analysis interview take home challenge to explore user logins and user data of a ridesharing company',
+    author='Juichia Che Holland',
+    license='MIT',
+)
